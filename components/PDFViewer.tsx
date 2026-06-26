e40dbbb91e0417a2f7431ad5084ae086cfb3933a
Karthikeyan <demobotishere@gmail.com>
2026-06-26 14:57:50 +0530

    Fix react-pdf CSS import path

--- components/PDFViewer.tsx
+++ components/PDFViewer.tsx
@@ -2,8 +2,8 @@
 
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 // Set up worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
